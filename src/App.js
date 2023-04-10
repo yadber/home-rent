7110d1e9b5694a95438393f65bb27fa8e844fd83
yadber <yadber58@gmail.com>
2023-04-10 09:00:17 +0300

    react-route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from "./pages/Offers";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
-    <div>
-      <p className='text-3xl font-bold underline'>Hello world</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/profile' element={<Profile />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='text-center text-2xl bg-blue-400'>
+      Home
+    </div>
+  )
+}
